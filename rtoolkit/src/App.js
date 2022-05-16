fa81208d5d666d282a7559ea62bfd71e366ecffc
ThomasMarik <Sunracer1977@gmail.com>
2022-05-16 11:54:34 -0400

    toolkit demo

---
+++ rtoolkit/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
+
+const App = () => {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <h1>count is :{count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        increment by 33
+      </button>
+    </div>
+  );
+};
+
+export default App;
